8905e9365a1f32b34eda10445f4723179aacacc4
Junior Rodrigues <junior.luiz182@gmail.com>
2021-02-25 12:02:16 -0300

    criando novos contexts e funçoes, sons de notificaçao

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/oLuizJunior.png" alt="foto do perfil"/>
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Luiz Carlos</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, ReactNode } from 'react';
+import { createContext, useState, ReactNode, useEffect } from 'react';
 import challenges from '../../challenges.json';
 
 interface Challenge {
@@ -15,7 +15,8 @@ interface ChallengesContextData {
     experienceToNextLevel: number;
     levelUp: () => void;
     startNewChallenge: () => void;
-    resetChallenge: () => void ;
+    resetChallenge: () => void;
+    completeChallenge: () => void;
 }
 
 interface ChallengesProviderProps {
@@ -33,6 +34,10 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 
     const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
+    useEffect(() => {
+        Notification.requestPermission();
+    }, [])
+
     function levelUp() {
         setLevel(level + 1)
     }
@@ -42,10 +47,37 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
         const challenge = challenges[randomChallengeIndex];
 
         setActiveChallenge(challenge)
+
+        new Audio('/notification.mp3').play();
+
+        if(Notification.permission === 'granted') {
+            new Notification('Novo desafio', {
+                body: `Valendo ${challenge.amount}xp!`
+            })
+        }
     }
 
     function resetChallenge() {
         setActiveChallenge(null);
+    }
+
+    function completeChallenge() {
+        if (!activeChallenge) {
+            return;
+        }
+
+        const { amount } = activeChallenge;
+
+        let finalExperience = currentExperience + amount;
+
+        if (finalExperience >= experienceToNextLevel) {
+            finalExperience = finalExperience - experienceToNextLevel;
+            levelUp();
+        }
+
+        setCurrentExperience(finalExperience);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
     }
 
      return (
@@ -59,6 +91,7 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
                 startNewChallenge, 
                 levelUp,
                 resetChallenge,
+                completeChallenge,
             }}
          >
              {children}
