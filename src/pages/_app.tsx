cd70a9145efbd66bc86bb267e664170939ce247c
Junior Rodrigues <junior.luiz182@gmail.com>
2021-02-26 12:07:34 -0300

    Criando modal, e armazenando dados no storage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,9 @@
 import '../styles/global.css';
-
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-import { CountdownProvider } from '../contexts/CountdownContext';
+import '../styles/global.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesProvider>
-      <CountdownProvider>
         <Component {...pageProps} />
-      </CountdownProvider>
-    </ChallengesProvider>
   )
 }
 
